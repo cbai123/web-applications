8719c7ae2b2a858488e365d5ac8ef380f31ee2a4
Chris <c.bailey00@hotmail.com>
2022-12-01 13:28:34 +0000

    finished phase_3 05 exercise and challenge

--- phase_2/music_library_database_app/app.rb
+++ phase_2/music_library_database_app/app.rb
@@ -20,6 +20,10 @@ class Application < Sinatra::Base
     return erb(:albums)
   end
 
+  get '/albums/new' do
+    return erb(:new_album)
+  end
+
   get '/albums/:id' do
     @album_id = params[:id]
     repo = AlbumRepository.new
@@ -34,20 +38,30 @@ class Application < Sinatra::Base
   end
 
   post '/albums' do
+    if invalid_request_parameters_albums?
+      status 400
+      return ''
+    end
+    @title = params[:title]
+    
     new_album = Album.new
-    new_album.title = params[:title]
+    new_album.title = @title
     new_album.release_year = params[:release_year]
     new_album.artist_id = params[:artist_id]
 
     repo = AlbumRepository.new
     repo.create(new_album)
-    return nil
+    return erb(:album_created)
   end
 
   get '/artists' do
     repo = ArtistRepository.new
     @artists = repo.all
     return erb(:artists)
+  end
+
+  get '/artists/new' do
+    return erb(:new_artist)
   end
 
   get '/artists/:id' do
@@ -60,13 +74,29 @@ class Application < Sinatra::Base
   end
 
   post '/artists' do
-    new_artist = Artist.new
-    repo = ArtistRepository.new
-    new_artist.name = params[:name]
-    new_artist.genre = params[:genre]
+    if invalid_request_parameters_artists?
+      status 400
+      return ''
+    else
+      @name = params[:name]
+      new_artist = Artist.new
+      repo = ArtistRepository.new
+      new_artist.name = @name
+      new_artist.genre = params[:genre]
 
-    repo.create(new_artist)
+      repo.create(new_artist)
 
-    return ''
+      return erb(:artist_created)
+    end
+  end
+
+  private
+
+  def invalid_request_parameters_albums?
+    params[:title] == nil || params[:release_year] == nil || params[:artist_id] == nil
+  end
+
+  def invalid_request_parameters_artists?
+    params[:name] == nil || params[:genre] == nil
   end
 end
--- phase_2/music_library_database_app/spec/integration/application_spec.rb
+++ phase_2/music_library_database_app/spec/integration/application_spec.rb
@@ -25,45 +25,6 @@ describe Application do
     end
   end
 
-  context "POST /albums" do
-    it 'returns 200 OK' do
-      # Assuming the post with id 1 exists.
-      response = post('/albums', title: "Voyage", release_year: 2022, artist_id: 2)
-
-      expect(response.status).to eq(200)
-
-      response = get('/albums/13')
-      expect(response.status).to eq(200)
-      expect(response.body).to include("Voyage")
-    end
-  end
-
-  context "GET /artists" do
-    it 'returns 200 OK' do
-      response = get('/artists')
-      expected_response = 'Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos'
-
-      expect(response.status).to eq(200)
-      expect(response.body).to include('<h1>Artists:</h1>')
-      expect(response.body).to include('Name: Pixies')
-      expect(response.body).to include('Artist Page')
-      expect(response.body).to include('Name: Taylor Swift')
-    end
-  end
-
-  context "POST /artists" do
-    it 'returns 200 OK' do
-      response = post('/artists', name: "Giveon", genre: "RnB")
-
-      expect(response.status).to eq(200)
-      expect(response.body).to eq('')
-      response = get('/artists')
-
-      expect(response.status).to eq(200)
-      expect(response.body).to include 'Giveon'
-    end
-  end
-
   context "GET /albums/id" do
     it 'returns 200 OK' do
       # Assuming the post with id 1 exists.
@@ -84,6 +45,69 @@ describe Application do
     end
   end
 
+  context "GET /albums/new" do
+    it "returns the form" do
+      response = get('/albums/new')
+
+      expect(response.status).to eq(200)
+
+      expect(response.body).to include("Add an album")
+      expect(response.body).to include('<form method = "POST" action = "/albums">')
+      expect(response.body).to include('<input type = "text" name = "title">')
+      expect(response.body).to include('<input type = "int" name = "release_year">')
+      expect(response.body).to include('<input type = "int" name = "artist_id">')
+
+      expect(response.body).to include('<input type = "submit" value = "Submit">')
+    end
+  end
+
+  context "POST /albums" do
+    it "creates album and returns confirmation screen" do
+      response = post('/albums', title: "Voyage", release_year: 2022, artist_id: 2)
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include("You added Voyage to albums")
+      
+      response = get('/albums')
+      expect(response.body).to include("Voyage")
+    end
+
+    it "creates album and returns confirmation screen with different name" do
+      response = post('/albums', title: "OK Computer", release_year: 1997, artist_id: 1)
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include("You added OK Computer to albums")
+
+      response = get('/albums')
+      expect(response.body).to include("OK Computer")
+    end
+
+    it "returns 400 if invalid input" do
+      response = post('/albums', title: "title", release_year: nil, artist_id: 1)
+
+      expect(response.status).to eq(400)
+    end
+
+    it "returns 400 if invalid input" do
+      response = post('/albums', ttitle: "title")
+
+      expect(response.status).to eq(400)
+    end
+  end
+
+  context "GET /artists" do
+    it 'returns 200 OK' do
+      response = get('/artists')
+      expected_response = 'Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<h1>Artists:</h1>')
+      expect(response.body).to include('Name: Pixies')
+      expect(response.body).to include('Artist Page')
+      expect(response.body).to include('Name: Taylor Swift')
+    end
+  end
+
   context "GET /artists/id" do
     it 'returns 200 OK' do
       # Assuming the post with id 1 exists.
@@ -100,6 +124,49 @@ describe Application do
       expect(response.status).to eq(200)
       expect(response.body).to include('<h1>Nina Simone</h1>')
       expect(response.body).to include('Genre: Pop')
+    end
+  end
+
+  context "GET /artists/new" do
+    it "returns 200 OK" do
+      response = get('/artists/new')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include("<form method = 'POST' action = '/artists'>")
+      expect(response.body).to include("<h1>Add an artist</h1>")
+      expect(response.body).to include("<input type = 'text' name = 'name'>")
+      expect(response.body).to include("<input type = 'text' name = 'genre'>")
+      expect(response.body).to include("<input type = 'submit' value = 'Submit'>")
+    end
+  end
+
+  context "POST /artists" do
+    it 'creates an artist and returns confirmation screen' do
+      response = post('/artists', name: "Giveon", genre: "RnB")
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('You added the artist: Giveon')
+      response = get('/artists')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include 'Giveon'
+    end
+
+    it 'creates an artist and returns confirmation screen with a different artist' do
+      response = post('/artists', name: "Oasis", genre: "rock")
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('You added the artist: Oasis')
+      response = get('/artists')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include 'Oasis'
+    end
+
+    it "returns 400 if input is invalid" do
+      response = post('/artists', invalid_name: 'Giveon')
+
+      expect(response.status).to eq(400)
     end
   end
 end
